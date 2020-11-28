16ca8cc347649cf0156d12afb25ba53e8bab8b1b
Jonathan Pyle <jhpyle@gmail.com>
2020-11-27 20:17:59 -0500

    edited setup.py

--- setup.py
+++ setup.py
@@ -9,42 +9,42 @@ from io import open
 
 from setuptools import setup
 
-from docassemble_pattern import __version__
+#from docassemble_pattern import __version__
 
 #---------------------------------------------------------------------------------------------------
 # "python setup.py zip" will create the zipped distribution and checksum.
 
-if sys.argv[-1] == "zip":
+# if sys.argv[-1] == "zip":
 
-    import zipfile
-    import hashlib
-    import re
+#     import zipfile
+#     import hashlib
+#     import re
 
-    n = "docassemble_pattern-%s.zip" % __version__
-    p = os.path.join(os.path.dirname(os.path.realpath(__file__)))
-    z = zipfile.ZipFile(os.path.join(p, "..", n), "w", zipfile.ZIP_DEFLATED)
-    for root, folders, files in os.walk(p):
-        for f in files:
-            f = os.path.join(root, f)
-            # Exclude private settings.
-            if f.endswith(os.path.join("web", "api.py")):
-                d = "#--- PRIVATE"
-                s = open(f, "r", encoding="utf-8").read().split(d)
-                x = open(f, "w", encoding="utf-8")
-                x.write(s[0])
-                x.close()
-            # Exclude revision history (.git).
-            # Exclude development files (.dev).
-            if not re.search(r"\.DS|\.git[^i]|\.pyc|\.dev|tmp", f):
-                z.write(f, os.path.join("docassemble_pattern-" + __version__, os.path.relpath(f, p)))
-            if f.endswith(os.path.join("web", "api.py")):
-                x = open(f, "w", encoding="utf-8")
-                x.write(d.join(s))
-                x.close()
-    z.close()
-    print(n)
-    print(hashlib.sha256(open(z.filename).read()).hexdigest())
-    sys.exit(0)
+#     n = "docassemble_pattern-%s.zip" % __version__
+#     p = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+#     z = zipfile.ZipFile(os.path.join(p, "..", n), "w", zipfile.ZIP_DEFLATED)
+#     for root, folders, files in os.walk(p):
+#         for f in files:
+#             f = os.path.join(root, f)
+#             # Exclude private settings.
+#             if f.endswith(os.path.join("web", "api.py")):
+#                 d = "#--- PRIVATE"
+#                 s = open(f, "r", encoding="utf-8").read().split(d)
+#                 x = open(f, "w", encoding="utf-8")
+#                 x.write(s[0])
+#                 x.close()
+#             # Exclude revision history (.git).
+#             # Exclude development files (.dev).
+#             if not re.search(r"\.DS|\.git[^i]|\.pyc|\.dev|tmp", f):
+#                 z.write(f, os.path.join("docassemble_pattern-" + __version__, os.path.relpath(f, p)))
+#             if f.endswith(os.path.join("web", "api.py")):
+#                 x = open(f, "w", encoding="utf-8")
+#                 x.write(d.join(s))
+#                 x.close()
+#     z.close()
+#     print(n)
+#     print(hashlib.sha256(open(z.filename).read()).hexdigest())
+#     sys.exit(0)
 
 #---------------------------------------------------------------------------------------------------
 # "python setup.py install" will install /docassemble_pattern in /site-packages.
